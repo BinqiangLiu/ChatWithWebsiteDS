9c2eb01d15ad1db69f779f3707f72eb32de17842
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-04 16:19:56 +0800

    Update HFApp.py

--- HFApp.py
+++ HFApp.py
@@ -110,17 +110,7 @@ text_splitter = CharacterTextSplitter(
 with st.sidebar:
     url = st.text_input("Insert The website URL")
     print("You want to chat with: "+url)
-    if url.strip().isspace(): 
-        st.write("Please enter valid URL first.")
-    elif url == "":
-        st.write("Please enter valid URL first.")
-    elif url.strip() == "":
-        st.write("Please enter valid URL first.")
-    elif url.isspace():
-        st.write("Please enter valid URL first.")
-    elif url=="exit":
-        st.write("Please enter valid URL first.")
-#    st.write("Caution: This app is built based on the English Version of CPEG (2010). For most recent version, please refer to the CNIPA official source.")
+ #    st.write("Caution: This app is built based on the English Version of CPEG (2010). For most recent version, please refer to the CNIPA official source.")
     st.write("Disclaimer: This app is for information purpose only. NO liability could be claimed against whoever associated with this app in any manner. User should consult a qualified legal professional for legal advice.")
     st.subheader("Enjoy Chatting!")
     st.sidebar.markdown("Contact: [aichat101@foxmail.com](mailto:aichat101@foxmail.com)")
