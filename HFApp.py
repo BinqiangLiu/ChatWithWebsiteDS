c83e90d3dad697fbcadf96f697b19e593baa53b9
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-04 14:48:39 +0800

    Update HFApp.py

--- HFApp.py
+++ HFApp.py
@@ -1,18 +1,14 @@
 #https://www.youtube.com/watch?v=ZlwtYmKlL_0
 
-import os
 import streamlit as st
-from dotenv import load_dotenv
-from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import WebBaseLoader
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.prompts.chat import (ChatPromptTemplate,
                                     HumanMessagePromptTemplate,
                                     SystemMessagePromptTemplate)
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Chroma
+#from langchain.vectorstores import Chroma
+import streamlit as st
+from langchain.chains.question_answering import load_qa_chain
 from langchain import PromptTemplate, LLMChain
 from langchain import HuggingFaceHub
 from PyPDF2 import PdfReader
@@ -22,13 +18,25 @@ import requests
 from pathlib import Path
 from time import sleep
 import torch
+import os
 import random
 import string
 from dotenv import load_dotenv
-# Load environment variables from .env file (Optional)
 load_dotenv()
 
-#OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
+st.title('🦜🔗 Chat With Website')
+st.subheader('Input your website URL, ask questions, and receive answers directly from the website.')
+
+st.set_page_config(page_title="CPEG (EN) AI Chat Assistant", layout="wide")
+st.subheader("China Patent Examination Guideline (EN) AI Chat Assistant")
+#st.write('---')
+
+
+user_question = st.text_input("Insert The website URL")
+
+css_file = "main.css"
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
 HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 model_id = os.getenv('model_id')
@@ -58,59 +66,143 @@ chain = load_qa_chain(llm=llm, chain_type="stuff")
 def generate_random_string(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))  
-
+    
 texts=""
 initial_embeddings=""
 db_embeddings = ""
 i_file_path=""
 file_path = ""
+wechat_image= "WeChatCode.jpg"
 
-def main():
-    # Set the title and subtitle of the app
-    st.title('🦜🔗 Chat With Website')
-    st.subheader('Input your website URL, ask questions, and receive answers directly from the website.')
+st.sidebar.markdown(
+    """
+    <style>
+    .blue-underline {
+        text-decoration: bold;
+        color: blue;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-    url = st.text_input("Insert The website URL")
+st.markdown(
+    """
+    <style>
+        [data-testid=stSidebar] [data-testid=stImage]{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+        }
+    </style>
+    """, unsafe_allow_html=True
+)
 
-    prompt = st.text_input("Ask a question (query/prompt)")
-    if st.button("Submit Query", type="primary"):
-        ABS_PATH: str = os.path.dirname(os.path.abspath(__file__))
-        DB_DIR: str = os.path.join(ABS_PATH, "db")
+with st.sidebar:
+    option = st.sidebar.selectbox("Select the content to Chat:", ("TOC: Table of Contents", "Part I: Preliminary Examination", "Part II: Substantive Examination", "Part III: Examination of International Applications Entering the National Phase", "Part IV: Examination of Requests for Reexamination and for Invalidation", "Part V: Processing of Patent Applications and Procedural Matters", "Index", "Annexes"))
+    if option == "TOC: Table of Contents":
+        file_path = os.path.join(os.getcwd(), "CPEGFullV2010ENContents.pdf")
+    elif option == "Part I: Preliminary Examination":
+        file_path = os.path.join(os.getcwd(), "CPEGFullV2010ENPartI.pdf")
+    elif option == "Part II: Substantive Examination":
+        file_path = os.path.join(os.getcwd(), "CPEGFullV2010ENPartII.pdf")
+    elif option == "Part III: Examination of International Applications Entering the National Phase":
+        file_path = os.path.join(os.getcwd(), "CPEGFullV2010ENPartIII.pdf")
+    elif option == "Part IV: Examination of Requests for Reexamination and for Invalidation":
+        file_path = os.path.join(os.getcwd(), "CPEGFullV2010ENPartIV.pdf")
+    elif option == "Part V: Processing of Patent Applications and Procedural Matters":
+        file_path = os.path.join(os.getcwd(), "CPEGFullV2010ENPartV.pdf")
+    elif option == "Index":
+        file_path = os.path.join(os.getcwd(), "CPEGFullV2010ENIndex.pdf")
+    elif option == "Annexes":
+        file_path = os.path.join(os.getcwd(), "CPEGFullV2010ENAnnexes.pdf")
+    else:
+        st.write("Choose which part to Chat first.")
+        st.stop()
+    st.write("Caution: This app is built based on the English Version of CPEG (2010). For most recent version, please refer to the CNIPA official source.")
+    st.write("Disclaimer: This app is for information purpose only. NO liability could be claimed against whoever associated with this app in any manner. User should consult a qualified legal professional for legal advice.")
+    st.subheader("Enjoy Chatting!")
+    st.sidebar.markdown("Contact: [aichat101@foxmail.com](mailto:aichat101@foxmail.com)")
+    st.sidebar.markdown('WeChat: <span class="blue-underline">pat2win</span>, or scan the code below.', unsafe_allow_html=True)
+    st.image(wechat_image)
+    st.sidebar.markdown('<span class="blue-underline">Life Enhancing with AI.</span>', unsafe_allow_html=True)      
+    try:        
+        with st.spinner("Preparing materials for you..."):
+            doc_reader = PdfReader(file_path)
+            raw_text = ''
+            for i, page in enumerate(doc_reader.pages):
+                text = page.extract_text()
+                if text:
+                    raw_text += text
+#            text_splitter = RecursiveCharacterTextSplitter(        
+            text_splitter = CharacterTextSplitter(        
+                separator = "\n",
+                chunk_size = 1000,
+                chunk_overlap  = 200, #striding over the text
+                length_function = len,
+            )
+            temp_texts = text_splitter.split_text(raw_text)
+            texts = temp_texts
+            initial_embeddings=get_embeddings(texts)
+            db_embeddings = torch.FloatTensor(initial_embeddings) 
+    except Exception as e:
+        st.write("Unknow error.")
+        print("Unknow error.")
+        st.stop()
 
-        # Load data from the specified URL
-        loader = WebBaseLoader(url)
-        data = loader.load()
+user_question = st.text_input("Enter your question & query CPEG (EN):")
 
-        # Split the loaded data
-        text_splitter = CharacterTextSplitter(separator='\n', 
-                                        chunk_size=500, 
-                                        chunk_overlap=40)
+if user_question !="":         
+    #st.write("Your question: "+user_question)
+    print("Your question: "+user_question)
+    print()
+else:
+#    st.write("Please enter your question first.")
+    print("Please enter your question first.")
+    st.stop()
 
-        docs = text_splitter.split_documents(data)
+q_embedding=get_embeddings(user_question)
+final_q_embedding = torch.FloatTensor(q_embedding)
 
-        # Create OpenAI embeddings
-        openai_embeddings = OpenAIEmbeddings()
+from sentence_transformers.util import semantic_search
+hits = semantic_search(final_q_embedding, db_embeddings, top_k=5)
 
-        # Create a Chroma vector database from the documents
-        vectordb = Chroma.from_documents(documents=docs, 
-                                        embedding=openai_embeddings,
-                                        persist_directory=DB_DIR)
+for i in range(len(hits[0])):
+    print(texts[hits[0][i]['corpus_id']])
+    print()
 
-        vectordb.persist()
+page_contents = []
+for i in range(len(hits[0])):
+    page_content = texts[hits[0][i]['corpus_id']]
+    page_contents.append(page_content)
 
-        # Create a retriever from the Chroma vector database
-        retriever = vectordb.as_retriever(search_kwargs={"k": 3})
+print(page_contents)
+print()
+temp_page_contents=str(page_contents)
+print()
+final_page_contents = temp_page_contents.replace('\\n', '') 
+print(final_page_contents)
+print()
+print("AI Thinking...Please wait a while to Cheers!")
+print()
 
-        # Use a ChatOpenAI model
-        llm = ChatOpenAI(model_name='gpt-3.5-turbo')
+random_string = generate_random_string(20)
 
-        # Create a RetrievalQA from the model and retriever
-        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
-
-        # Run the prompt and return the response
-        response = qa(prompt)
-        st.write(response)
-        
-
-if __name__ == '__main__':
-    main()
+with st.spinner("AI Thinking...Please wait a while to Cheers!"):
+    i_file_path = random_string + ".txt"
+    with open(i_file_path, "w", encoding="utf-8") as file:
+        file.write(final_page_contents)
+    loader = TextLoader(i_file_path, encoding="utf-8")
+    loaded_documents = loader.load()
+    temp_ai_response=chain.run(input_documents=loaded_documents, question=user_question)
+    final_ai_response=temp_ai_response.partition('<|end|>')[0]
+    i_final_ai_response = final_ai_response.replace('\n', '')
+    print("AI Response:")
+    print(i_final_ai_response)
+    print("Have more questions? Go ahead and continue asking your AI assistant : )")
+    st.write("AI Response:")
+    st.write(i_final_ai_response)
+#    st.write("---")
+#    st.write("Have more questions? Go ahead and continue asking your AI assistant : )")
