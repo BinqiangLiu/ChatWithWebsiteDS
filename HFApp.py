8a55b7ed5a863687ca2b6d88d8d073931fbe0943
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-05 11:58:21 +0800

    Update HFApp.py

--- HFApp.py
+++ HFApp.py
@@ -105,7 +105,8 @@ text_splitter = CharacterTextSplitter(
 )
 
 with st.sidebar:
-    url = st.text_input("Insert The website URL")
+    st.subheader("Enter website URL to AI-Chat!")    
+    url = st.text_input()
     print("You want to chat with: "+url)
     st.write("Disclaimer: This app is for information purpose only. NO liability could be claimed against whoever associated with this app in any manner. User should consult a qualified legal professional for legal advice.")
     st.subheader("Enjoy Chatting!")
